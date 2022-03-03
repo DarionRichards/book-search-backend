ff91e9aaa89c873fb2bf6f2154ecf7e7620ee58a
Darion Richards <darionrichards@outlook.com>
2022-03-03 18:20:49 +0000

    Seeded data for users

---
+++ src/seeds/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+import User from "../models/index.js";
+
+import {userSeed} from "./data/userSeed.js";
+
+const init = async () => {
+	try {
+		await mongoose.connect("mongodb://localhost:27017/bookSearchDb", {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+
+		console.log("[INFO]: Connection to DB successful");
+
+		await User.deleteMany({});
+		await User.insertMany(userSeed);
+
+		console.log("[INFO]: Successfully seeded users");
+
+		await mongoose.disconnect();
+	} catch (error) {
+		console.log(`[ERROR]: Connection to DB unsuccessful | ${error.message}`);
+	}
+};
+
+init();
